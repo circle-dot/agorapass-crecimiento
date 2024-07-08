2230c8e7b9144abc95a210e8d088f74587f5f4d5
Matias <marazza.matias@gmail.com>
2024-07-07 22:43:07 -0300

    Better search bar

--- src/app/agora/page.tsx
+++ src/app/agora/page.tsx
@@ -8,10 +8,11 @@ import {
 } from "@/components/ui/card"
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import SearchBar from "@/components/ui/users/searchBar";
 
 export default function Home() {
     return (
-        <div className="relative flex items-center justify-center min-h-screen bg-cover bg-center bg-no-repeat bg-primarydark" >
+        <div className="relative flex items-center justify-center min-h-screen bg-cover bg-center bg-no-repeat bg-[url('/agora-bg.png')]" >
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <main className="relative z-10 flex items-center justify-center min-h-screen w-full mx-auto p-5 md:max-w-lg md:p-7.5 lg:max-w-2xl lg:p-10">
                 <div className="flex flex-col items-center justify-center w-full">
@@ -25,9 +26,9 @@ export default function Home() {
                                 className="select-none pointer-events-none"
                             />
                             <CardTitle className="text-center uppercase tracking-[3.5px] text-4xl font-extralight pb-4">Agorapass</CardTitle>
+                            <SearchBar />
                             <CardDescription className="text-center w-full flex justify-evenly items-center flex-col gap-y-2">
-                                <Button asChild className="text-xl"><Link href='/agora/profiles'>Start vouching!</Link></Button>
-                                <p>Or check something</p>
+                                <Link href='/agora/profiles' className="underline">Or check some profiles</Link>
                             </CardDescription>
                         </CardHeader>
                         <CardContent>
--- src/components/ui/users/searchBar.tsx
+++ src/components/ui/users/searchBar.tsx
@@ -1,13 +1,16 @@
+"use client"
 import React, { useState, useEffect } from "react";
-import { ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client";
+import { gql, useLazyQuery } from "@apollo/client";
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import client from "@/lib/ApolloClient";
+import Link from 'next/link';
 
 const SEARCH_ENS_NAMES = gql`
   query SearchEnsNames($query: String!) {
     ensNames(where: { name: { contains: $query } }) {
       name
+      id
     }
   }
 `;
@@ -33,79 +36,99 @@ const SEARCH_WALLETS = gql`
 const SearchBar = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [searchResults, setSearchResults] = useState<any[]>([]);
-    const [selectedQuery, setSelectedQuery] = useState("ENS Names");
 
-    const { loading: loadingEns, error: errorEns, data: dataEns } = useQuery(SEARCH_ENS_NAMES, {
-        variables: { query: searchTerm },
+    const [searchEnsNames, { loading: loadingEns, error: errorEns, data: dataEns }] = useLazyQuery(SEARCH_ENS_NAMES, {
         client,
-        skip: !searchTerm || selectedQuery !== "ENS Names",
     });
 
-    const { loading: loadingAttestations, error: errorAttestations, data: dataAttestations } = useQuery(SEARCH_ATTESTATIONS, {
-        variables: { query: searchTerm },
+    const [searchAttestations, { loading: loadingAttestations, error: errorAttestations, data: dataAttestations }] = useLazyQuery(SEARCH_ATTESTATIONS, {
         client,
-        skip: !searchTerm || selectedQuery !== "Attestations",
     });
 
-    const { loading: loadingWallets, error: errorWallets, data: dataWallets } = useQuery(SEARCH_WALLETS, {
-        variables: { query: searchTerm },
+    const [searchWallets, { loading: loadingWallets, error: errorWallets, data: dataWallets }] = useLazyQuery(SEARCH_WALLETS, {
         client,
-        skip: !searchTerm || selectedQuery !== "Wallet Addresses",
     });
 
     useEffect(() => {
-        if (selectedQuery === "ENS Names" && dataEns) {
-            setSearchResults(dataEns.ensNames.map((ensName: any) => ({
-                name: ensName.name,
-            })));
-        } else if (selectedQuery === "Attestations" && dataAttestations) {
-            setSearchResults(dataAttestations.attestations.map((attestation: any) => ({
-                id: attestation.id,
-                recipient: attestation.recipient,
-            })));
-        } else if (selectedQuery === "Wallet Addresses" && dataWallets) {
-            // Use a Set to ensure unique wallet addresses
-            const uniqueWallets = Array.from(new Set(dataWallets.attestations.map((wallet: any) => wallet.recipient)));
-            setSearchResults(uniqueWallets.map((wallet: any) => ({
-                recipient: wallet,
-            })));
+        if (searchTerm) {
+            searchEnsNames({ variables: { query: searchTerm } });
+            searchAttestations({ variables: { query: searchTerm } });
+            searchWallets({ variables: { query: searchTerm } });
         }
-    }, [dataEns, dataAttestations, dataWallets, selectedQuery]);
+    }, [searchTerm, searchEnsNames, searchAttestations, searchWallets]);
+
+    useEffect(() => {
+        const results = new Map();
+
+        if (dataEns) {
+            dataEns.ensNames.forEach((ensName: any) => {
+                results.set(ensName.id.toLowerCase(), {
+                    type: "ENS Name",
+                    value: ensName.name,
+                    id: ensName.id.toLowerCase(), // Normalize address to lowercase
+                });
+            });
+        }
+
+        if (dataAttestations) {
+            dataAttestations.attestations.forEach((attestation: any) => {
+                results.set(attestation.id.toLowerCase(), {
+                    type: "Attestation ID",
+                    value: attestation.id,
+                    recipient: attestation.recipient.toLowerCase(), // Normalize address to lowercase
+                });
+            });
+        }
+
+        if (dataWallets) {
+            const uniqueWallets = Array.from(new Set(dataWallets.attestations.map((wallet: any) => wallet.recipient.toLowerCase())));
+            uniqueWallets.forEach((wallet: any) => {
+                results.set(wallet, {
+                    type: "Wallet Address",
+                    value: wallet,
+                });
+            });
+        }
+
+        setSearchResults(Array.from(results.values()));
+    }, [dataEns, dataAttestations, dataWallets]);
+
+    const getLinkForResult = (result: any) => {
+        if (result.type === "Wallet Address") {
+            return `/agora/address/${result.value}`;
+        } else if (result.type === "ENS Name") {
+            return `/agora/address/${result.id}`; // Use id for the ENS Name link
+        } else if (result.type === "Attestation ID") {
+            return `/agora/vouch/${result.value}`;
+        }
+        return "#";
+    };
 
     return (
         <div className="relative">
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
                 type="search"
-                placeholder="Search ENS Names, Attestations, or Wallet Addresses..."
+                placeholder="Search ENS Names, Wallets or Attestations..."
                 className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <select
-                onChange={(e) => setSelectedQuery(e.target.value)}
-                className="absolute top-10 left-0 p-2 border border-gray-300 rounded"
-            >
-                <option value="ENS Names">ENS Names</option>
-                <option value="Attestations">Attestations</option>
-                <option value="Wallet Addresses">Wallet Addresses</option>
-            </select>
             {searchTerm && (
                 <div className="absolute z-10 mt-1 w-full bg-white border border-gray-300 rounded shadow-lg">
                     {(loadingEns || loadingAttestations || loadingWallets) && <p>Loading...</p>}
-                    {(errorEns || errorAttestations || errorWallets) && <p>Error: {errorEns?.message || errorAttestations?.message || errorWallets?.message}</p>}
+                    {(errorEns || errorAttestations || errorWallets) && (
+                        <p>Error: {errorEns?.message || errorAttestations?.message || errorWallets?.message}</p>
+                    )}
                     {searchResults.map((result, index) => (
-                        <div key={index} className="p-2 hover:bg-gray-200">
-                            {selectedQuery === "ENS Names" ? (
-                                <p>ENS Name: {result.name}</p>
-                            ) : selectedQuery === "Attestations" ? (
-                                <>
-                                    <p>Attestation ID: {result.id}</p>
-                                </>
-                            ) : (
-                                <p>Wallet Address: {result.recipient}</p>
-                            )}
-                        </div>
+                        <Link key={index} href={getLinkForResult(result)} passHref>
+                            <p className="block p-2 hover:bg-gray-200">
+                                <p>
+                                    {result.type}: {result.value}
+                                </p>
+                                {result.recipient && <p>Recipient: {result.recipient}</p>}
+                            </p>
+                        </Link>
                     ))}
                 </div>
             )}
