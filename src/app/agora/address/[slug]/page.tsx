d383f9570cc07d249078783617cec95ccecee74c
Matias <marazza.matias@gmail.com>
2024-07-09 12:41:32 -0300

    attestation page changes

--- src/app/agora/address/[slug]/page.tsx
+++ src/app/agora/address/[slug]/page.tsx
@@ -42,7 +42,7 @@ export default function Page({ params }: { params: { slug: string } }) {
     if (madeError || receivedError || ensNameerror) return <div>Error: {madeError?.message || receivedError?.message}</div>;
 
     return (
-        <div className="flex items-center justify-center flex-grow bg-gray-100">
+        <div className="flex items-center justify-center bg-gray-100 w-full min-h-screen p-4">
             <motion.div
                 initial={{
                     opacity: 0,
@@ -56,10 +56,10 @@ export default function Page({ params }: { params: { slug: string } }) {
                     duration: 0.5,
                     ease: [0.4, 0.0, 0.2, 1],
                 }}
-                className="bg-white bg-opacity-90 shadow-lg mt-8 flex flex-col items-center justify-center w-full max-w-4xl p-6 rounded-lg"
+                className="bg-white bg-opacity-90 shadow-lg p-6 rounded-lg max-w-md w-full"
             >
-                <div className="flex justify-center mt-5">
-                    <div className="h-32 rounded-full">
+                <div className="flex justify-center mb-6">
+                    <div className="h-32 w-32 rounded-full overflow-hidden">
                         <Image
                             src={imageLogo}
                             width={100}
@@ -69,23 +69,25 @@ export default function Page({ params }: { params: { slug: string } }) {
                         />
                     </div>
                 </div>
-                <div className="text-center py-6 px-4">
-                    <div className="mt-6 flex justify-center space-x-8">
-                        <div>
-                            <span className="block text-xl font-bold">{receivedData}</span>
-                            <span className="text-gray-500">Vouches received</span>
-                        </div>
-                        <div>
-                            <span className="block text-xl font-bold">{madeData}</span>
-                            <span className="text-gray-500">Vouches made</span>
+                <div className="text-center">
+                    <div className="flex flex-col gap-4 items-center">
+                        <div className="flex flex-col sm:flex-row sm:space-x-8">
+                            <div>
+                                <span className="block text-xl font-bold">{receivedData}</span>
+                                <span className="text-gray-500 text-sm">Vouches received</span>
+                            </div>
+                            <div>
+                                <span className="block text-xl font-bold">{madeData}</span>
+                                <span className="text-gray-500 text-sm">Vouches made</span>
+                            </div>
                         </div>
                     </div>
-                    <div className="text-center mt-6">
+                    <div className="mt-6">
                         <h3 className="text-2xl font-semibold truncate">
                             <TooltipProvider>
                                 <Tooltip>
                                     <TooltipTrigger asChild>
-                                        <p>{ensName?.length > 0 ? ensName[0].name : params.slug || "No Data Available"}</p>
+                                        <p className="whitespace-nowrap">{ensName?.length > 0 ? ensName[0].name : params.slug || "No Data Available"}</p>
                                     </TooltipTrigger>
                                     <TooltipContent>
                                         <p>{ensName?.length > 0 ? ensName[0].name : params.slug || "No Data Available"}</p>
--- src/app/agora/attestation/[slug]/page.tsx
+++ src/app/agora/attestation/[slug]/page.tsx
@@ -1,7 +1,17 @@
 "use client";
+
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchAttestation } from "@/lib/fetchers/attestations";
+import { motion } from "framer-motion";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function Page({ params }: { params: { slug: string } }) {
     // Fetch attestation data
@@ -24,23 +34,125 @@ export default function Page({ params }: { params: { slug: string } }) {
 
     // Once data is loaded, display the attestation details
     return (
-        <div>
-            <h1>Attestation Details</h1>
-            <div>
-                <p>ID: {data.id}</p>
-                <p>Data: {data.data}</p>
-                <p>Decoded Data: {JSON.stringify(data.decodedDataJson)}</p>
-                <p>Attester: {data.attester}</p>
-                <p>Time: {new Date(data.time * 1000).toLocaleString()}</p>
-                <p>Time Created: {new Date(data.timeCreated * 1000).toLocaleString()}</p>
-                <p>Recipient: {data.recipient}</p>
-                <p>Expiration Time: {data.expirationTime ? new Date(data.expirationTime * 1000).toLocaleString() : "N/A"}</p>
-                <p>Revocation Time: {data.revocationTime ? new Date(data.revocationTime * 1000).toLocaleString() : "N/A"}</p>
-                <p>Ref UID: {data.refUID}</p>
-                <p>Revocable: {data.revocable ? "Yes" : "No"}</p>
-                <p>Revoked: {data.revoked ? "Yes" : "No"}</p>
-                <p>Schema ID: {data.schemaId}</p>
-            </div>
+        <div className="flex items-center justify-center bg-gray-100 w-full min-h-screen p-4">
+            <motion.div
+                initial={{
+                    opacity: 0,
+                    y: 20,
+                }}
+                animate={{
+                    opacity: 1,
+                    y: [20, -5, 0],
+                }}
+                transition={{
+                    duration: 0.5,
+                    ease: [0.4, 0.0, 0.2, 1],
+                }}
+                className="bg-white bg-opacity-90 shadow-lg p-6 rounded-lg max-w-md w-full"
+            >
+                <TooltipProvider>
+                    <Card className="bg-white w-full">
+                        <CardHeader className="text-center">
+                            <CardTitle className="text-2xl font-semibold">Attestation Details</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="mt-4 space-y-4">
+                                {/* ID Field with Tooltip */}
+                                <div className="flex items-center justify-between">
+                                    <span className="font-semibold">ID:</span>
+                                    <div className="truncate w-1/2">
+                                        <Tooltip>
+                                            <TooltipTrigger asChild>
+                                                <p className="text-link truncate">{data.id}</p>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>{data.id}</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </div>
+                                </div>
+                                {/* Attester Field with Tooltip */}
+                                <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Attester:</span>
+                                    <div className="truncate w-1/2">
+                                        <Tooltip>
+                                            <TooltipTrigger asChild>
+                                                <Link href={'/agora/address/' + data.attester} className="text-link block truncate">{data.attester}</Link>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>{data.attester}</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </div>
+                                </div>
+                                {/* Time Field */}
+                                <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Time:</span>
+                                    <span className="text-gray-700">{new Date(data.time * 1000).toLocaleString()}</span>
+                                </div>
+                                {/* Time Created Field */}
+                                <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Time Created:</span>
+                                    <span className="text-gray-700">{new Date(data.timeCreated * 1000).toLocaleString()}</span>
+                                </div>
+                                {/* Recipient Field with Tooltip */}
+                                <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Recipient:</span>
+                                    <div className="truncate w-1/2">
+                                        <Tooltip>
+                                            <TooltipTrigger asChild>
+                                                <Link href={'/agora/address/' + data.recipient} className="text-link block truncate">{data.recipient}</Link>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>{data.recipient}</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </div>
+                                </div>
+                                {/* Expiration Time Field */}
+                                <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Expiration Time:</span>
+                                    <span className="text-gray-700">{data.expirationTime ? new Date(data.expirationTime * 1000).toLocaleString() : "N/A"}</span>
+                                </div>
+                                {/* Revocation Time Field */}
+                                <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Revocation Time:</span>
+                                    <span className="text-gray-700">{data.revocationTime ? new Date(data.revocationTime * 1000).toLocaleString() : "N/A"}</span>
+                                </div>
+                                {/* Ref UID Field */}
+                                {/* <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Ref UID:</span>
+                                    <span className="text-gray-700 truncate">{data.refUID}</span>
+                                </div> */}
+                                {/* Revocable Field */}
+                                <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Revocable:</span>
+                                    <span className="text-gray-700">{data.revocable ? "Yes" : "No"}</span>
+                                </div>
+                                {/* Revoked Field */}
+                                <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Revoked:</span>
+                                    <span className="text-gray-700">{data.revoked ? "Yes" : "No"}</span>
+                                </div>
+                                {/* Schema ID Field with Tooltip */}
+                                {/* <div className="flex items-center justify-between">
+                                    <span className="font-semibold">Schema ID:</span>
+                                    <div className="truncate w-1/2">
+                                        <Tooltip>
+                                            <TooltipTrigger asChild>
+                                                <p className="text-link truncate">{data.schemaId}</p>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>{data.schemaId}</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </div>
+                                </div> */}
+                            </div>
+                        </CardContent>
+                    </Card>
+                </TooltipProvider>
+            </motion.div>
         </div>
     );
 }
