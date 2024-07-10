59608b0552fbab631969f06bcce8682c3d9fccb1
Matias <marazza.matias@gmail.com>
2024-07-10 06:37:07 -0300

    Fix dependencies

--- src/lib/privy.ts
+++ src/lib/privy.ts
@@ -1,4 +1,12 @@
 import { PrivyClient } from '@privy-io/server-auth';
 
-const privy = new PrivyClient(process.env.NEXT_PUBLIC_PRIVY_APP_ID, process.env.PRIVY_APP_SECRET);
-export default privy
+const privyAppId = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
+const privyAppSecret = process.env.PRIVY_APP_SECRET;
+
+if (!privyAppId || !privyAppSecret) {
+    throw new Error('Privy environment variables are not set.');
+}
+
+const privy = new PrivyClient(privyAppId, privyAppSecret);
+
+export default privy;
