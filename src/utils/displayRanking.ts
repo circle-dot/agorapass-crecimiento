9664809840c61ad16bfb58da5fadffb8bd60546f
MarazzaM <marazza.matias@gmail.com>
2024-08-27 14:26:36 -0300

    Update scaling rankScore display

--- src/utils/displayRanking.ts
+++ src/utils/displayRanking.ts
@@ -1,5 +1,5 @@
 export default function displayRanking(ranking: string) {
-    const numberToMultiply = 1000;
+    const numberToMultiply = 100;
     const normalizedRanking = parseFloat(ranking) * numberToMultiply;
     const roundedNumber = Math.floor(normalizedRanking);
     return roundedNumber;
